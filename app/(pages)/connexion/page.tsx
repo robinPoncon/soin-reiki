a8e5932757a6f1806ee6bd321026e072745be597
robinPoncon <poncon.robin@gmail.com>
2023-09-15 18:13:58 +0200

    add breakpoint + begin responsive with mobile first concept

--- app/(pages)/connexion/page.tsx
+++ app/(pages)/connexion/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const LoginPage = () => {
 	return (
-		<section className="flex flex-col gap-12 py-20 px-24">
-			<h1 className="text-4xl mx-auto w-fit customBorderPink p-4">
+		<section className="flex flex-col gap-12 py-20 px-2 lg:px-24">
+			<h1 className="font-semibold sm:text-2xl md:text-4xl mx-auto w-fit customBorderPink p-4">
 				Nouvelle fonctionnalité à venir !
 			</h1>
-			<div className="flex justify-center gap-12">
+			<div className="flex flex-col mx-auto md:flex-row justify-center gap-12">
 				<Image
 					className="customImgPink"
 					alt="illustration d'un tableau de bord"
@@ -14,7 +14,7 @@ const LoginPage = () => {
 					width={320}
 					height={320}
 				/>
-				<p className="max-w-sm my-auto text-justify">
+				<p className="text-justify mx-auto my-auto max-w-xs px-2 sm:px-0 lg:max-w-sm">
 					Bientôt, je vais intégrer un tableau de bord de suivi sur le site. Cette
 					fonctionnalité vous permettra, entre autres, de suivre de près votre progression
 					au fil de nos séances, de consulter votre planning avec les rendez-vous déjà
