04e175c06036658f7e59886f910ffcad0f4de099
robinPoncon <poncon.robin@gmail.com>
2023-09-18 10:51:22 +0200

    add responsive cookie bar

--- app/_components/CookieBar/CookieBar.tsx
+++ app/_components/CookieBar/CookieBar.tsx
@@ -21,18 +21,16 @@ const CookieBar = () => {
 	};
 
 	return consent === null ? (
-		<div className="cookieBarComponent">
-			<div className="text-sm">
+		<div className="cookieBarComponent flex-col md:flex-row md:gap-10 lg:gap-20 xl:gap-80 xl:px-5">
+			<div className="text-sm text-justify">
 				<p>
 					J'utilise des cookies pour vous offrir la meilleure expérience possible sur mon
-					site.
-				</p>
-				<p>
-					En cliquant sur "Accepter", vous consentez à l'utilisation de tous les cookies.
-					Si vous préférez ne pas les accepter, vous pouvez choisir de les refuser.
+					site. En cliquant sur "Accepter", vous consentez à l'utilisation de tous les
+					cookies. Si vous préférez ne pas les accepter, vous pouvez choisir de les
+					refuser.
 				</p>
 			</div>
-			<div className="flex gap-10 mr-10">
+			<div className="flex gap-10 mt-5 justify-center md:mt-0">
 				<button onClick={handleAccept}>Accepter</button>
 				<button onClick={handleDecline}>Refuser</button>
 			</div>
