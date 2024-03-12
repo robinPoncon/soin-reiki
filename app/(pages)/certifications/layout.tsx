8f735174a6414dfa577b2f6aba7a439fb0df55a4
robinPoncon <poncon.robin@gmail.com>
2024-03-12 17:36:55 +0100

    fix spelling mistake

--- app/(pages)/certifications/layout.tsx
+++ app/(pages)/certifications/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
 	title: "Certifications Reiki & Tirage de Cartes",
 	description:
-		"Découvrezw mes certifications en Reiki et tirage de cartes qui attestent de mon expertise et de mes compétences dans ces domaines.",
+		"Découvrez mes certifications en Reiki et tirage de cartes qui attestent de mon expertise et de mes compétences dans ces domaines.",
 	keywords: [
 		"certifications Reiki",
 		"certification tirage de cartes",
