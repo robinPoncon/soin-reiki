72ce17aed19ceb83d092d24fe7e05fed8e3d924e
robinPoncon <poncon.robin@gmail.com>
2024-03-14 19:00:53 +0100

    maj google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,19 +62,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="fr">
-			<Script
-				src="https://www.googletagmanager.com/gtag/js?id=AW-755379002"
-				strategy="afterInteractive"
-			></Script>
-			<Script
-				id="google-analytics"
-				strategy="afterInteractive"
-			>
-				{`window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());gtag('config', 'AW-755379002');
-				`}
+			<Script>
+				{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-TPNK5DLH');`}
 			</Script>
+
 			<body
 				className={`font-helvetica font-normal pt-20 bg-customWhite text-darkTurquoise ${handleeFont.variable}`}
 			>
